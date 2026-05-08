cfba143178850a3a5b128a43dd26eb5bebe055c9
David Delgado <drd456delgado@gmail.com>
2026-05-08 14:09:52 -0700

    Initial Commit

---
+++ LifeOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeOS
+//
+//  Created by David Delgado on 5/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LifeOS/LifeOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeOSApp.swift
+//  LifeOS
+//
+//  Created by David Delgado on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct LifeOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
